f3fe6101b404d32d157fed0ba0ae3f9f9161e1d4
Bryan Westcott <101424913+bryan-westcott@users.noreply.github.com>
2025-06-06 10:56:04 -0500

    Fix create_symlinks for parent link and sed substitution

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -17,22 +17,24 @@ dest_root="${project_root?}/${fixed_dir?}"
 # === PREP ===
 
 cd "${project_root?}"
-echo "Building symlinked package under: ${dest_root?}"
+echo "-- Building symlinked package under: ${dest_root?} --"
 mkdir -p "${dest_root?}"
 
 
 # === LINK ROOT-LEVEL FILES TO DEST_ROOT ===
 
+echo "-- Creating symlinks for files in project root: ${project_root?} --"
 find "${project_root?}" -maxdepth 1 -type f -name '*.py' | while read -r file; do
-  filename=$(basename "$file")
-  dest="${dest_root?}/$filename"
-
-  symlink_target=$(realpath --relative-to="${dest_root?}" "$file")
-  ln -sf "$symlink_target" "$dest"
+    filename=$(basename "$file")
+    dest="${dest_root?}/$filename"
+    symlink_target=$(realpath --relative-to="${dest_root?}" "$file")
+    echo "    * linking: ${dest#"${project_root?}"} -> $symlink_target"
+    ln -sf "$symlink_target" "$dest"
 done
 
 # === SYM LINK IN SRC ALSO, PRESERVING STRUCTURE ===
 
+echo "-- Creating symlinks for tree in ${src_root?} --"
 find "${src_root?}" -type f -name '*.py' | while read -r file; do
     # Note: we will map anything in <projectroot> or src_dir to dest_root
     relpath="${file#"${src_root?}"/}"  # strip '*/src/' prefix, if exists
@@ -43,24 +45,29 @@ find "${src_root?}" -type f -name '*.py' | while read -r file; do
 
     # compute relative symlink target
     symlink_target=$(realpath --relative-to="$(dirname "$dest")" "$file")
+    echo "    * linking: ${dest#"${project_root?}"} -> $symlink_target"
     ln -sf "${symlink_target?}" "${dest?}"
 done
 
 
 # === ADD INIT IN EACH DIR ===
 
+echo "-- Creating __init__.py in each directory of ${dest_root?} --"
 find "${dest_root?}" -type d -exec touch {}/__init__.py \;
 
 
 # === REWRITING IMPORTS IN *ORIGINAL* FILES ===
 
-echo "Rewriting 'from src.' -> 'from ${project_name?}.' in source files..."
-find "${src_dir?}" -type f -name '*.py' | while read -r file; do
+echo "-- Rewriting 'from src.' -> 'from ${project_name?}.' in source files --"
+find "${dest_root?}" -type l -name '*.py' | while read -r file; do
+# only follow links (type l), the __init__.py files are not links
     # in-place, safe sed: modify import
+    realfile="$(realpath "$file")"
+    echo "    * checking ${realfile#"${project_root?}"} for src. imports"
     sed -i \
         -e "s|\bfrom src\\.|from ${project_name?}.|g" \
         -e "s|\bimport src\\.|import ${project_name?}.|g" \
-        "${file?}"
+        "${realfile?}"
 done
 
-echo "Symlinks created and imports rewritten to use '${project_name?}' namespace."
+echo "-- Symlinks created and imports rewritten to use '${project_name?}' namespace --"
